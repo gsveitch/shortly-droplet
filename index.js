a064ef56a0df1b1eebd14998750fc473769eca03
Gareth <gsveitch@gmail.com>
2017-12-16 12:01:25 -0600

    require dotenv in index.js for port snafu

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var app = require('./shortly.js');
+const env = require('dotenv').config({ path: '/Users/garethveitch/Documents/OpSpark/Immersion/Week-5/shortly-droplet'});
+
 const port = process.env.PORT || 4568;
 app.listen(port, function() {
   console.log('Shortly is listening on process.env.PORT ' + port);
