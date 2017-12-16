9703e61198d01ceec3c3403b62513b684fe34146
Gareth <gsveitch@gmail.com>
2017-12-16 12:15:13 -0600

    fix the port situation? maybe?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('./shortly.js');
 const env = require('dotenv').config();
 
-const port = process.env.PORT || 4568;
+const port = process.env.PORT || 80;
 app.listen(port, function() {
-  console.log('Shortly is listening on process.env.PORT ' + port);
+  console.log('Shortly is listening on process.env.PORT ' + port + ' || ' + port);
 });
